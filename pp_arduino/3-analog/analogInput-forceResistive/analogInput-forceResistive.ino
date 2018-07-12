6c9c398c49ae3f2d81e6faf470d16ad22c866e4e
tabreturn <github@tabreturn.com>
2018-07-12 18:24:38 +1200

    - add analog arduino sketches

---
+++ pp_arduino/3-analog/analogInput-forceResistive/analogInput-forceResistive.ino
@@ -0,0 +1,21 @@
+// analog Input : Force Resistive Sensor Interface
+// 1 FRS, 1 breadboard, hook-up wires, 1 10kOhm resistor
+// For a schematic visit https://learn.adafruit.com/force-sensitive-resistor-fsr/using-an-fsr
+
+
+int reading; // reading from analog pin
+int highest = 512; // highest value assumed 512
+int output; // 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  reading = analogRead(A0);
+  highest = max(reading, highest);
+  output = map(reading, 0, highest, 0, 1024);
+  Serial.println(output);
+}
